c9ce05f2a52db4c26c61b108b91dce4e817b40db
faustojc <faustojcboko@gmail.com>
2022-12-30 11:05:47 +0800

    Added composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'composer/bin',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7028275453a83ef67bdb67494ba9d4647de538b2',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'composer/bin' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7028275453a83ef67bdb67494ba9d4647de538b2',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
