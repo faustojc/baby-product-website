f493c1f80ec5ac2ac3a75308176c089dcc4fb94c
faustojc <faustojcboko@gmail.com>
2023-01-04 17:41:19 +0800

    Removed the footer

--- index.php
+++ index.php
@@ -104,59 +104,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 				</form>
 			</div>
         </div>
-        <!-- Footer -->
-        <footer class="bg-body py-4 mt-5 w-100 border-top border-secondary" style="border-width: 20px!important;">
-            <div class="container">
-                <div class="row justify-content-between align-items-center">
-                    <div class="col-md-3">
-                        <h3>AngelCare</h3>
-                        <p class="mb-2">Providing your baby needs.</p>
-                        <p class="mb-2">
-                            Change theme:
-                            <span id="changeTheme" class="badge text-wrap text-bg-primary w-auto" role="button"></span>
-                        </p>
-                    </div>
-                    <div class="col-md-3">
-                        <h3>Links</h3>
-                        <ul class="list-unstyled">
-                            <li><a href="#" class="text-decoration-none">Home</a></li>
-                            <li><a href="#" class="text-decoration-none">About</a></li>
-                            <li><a href="#" class="text-decoration-none">Contact</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-3">
-                        <h3>Contact Us</h3>
-                        <p>
-                            123 Main Street<br>
-                            Anytown, USA 12345
-                        </p>
-                        <p>
-                            Phone: 555-555-5555<br>
-                            Email: info@example.com
-                        </p>
-                    </div>
-                    <div class="col-md-3">
-                        <h3>Social</h3>
-                        <ul class="list-unstyled">
-                            <li><a href="#" class="text-decoration-none">Facebook</a></li>
-                            <li><a href="#" class="text-decoration-none">Twitter</a></li>
-                            <li><a href="#" class="text-decoration-none">Instagram</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="row mt-5">
-                    <div class="col text-center">
-                        <p>Copyright &copy; 2022 Bar Pink</p>
-                    </div>
-                </div>
-            </div>
-        </footer>
     </div>
-
 
     <script defer src="js/main.js"></script>
     <script defer src="js/form.js"></script>
-    <script defer src="js/changeTheme.js"></script>
     <script defer src="js/bootstrap/bootstrap.bundle.min.js"></script>
 </body>
 
