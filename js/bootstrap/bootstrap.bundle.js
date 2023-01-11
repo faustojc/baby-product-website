2794090d5cba59ed1de079d3a381325b086fd6c4
faustojc <faustojcboko@gmail.com>
2023-01-11 22:44:14 +0800

    Fixed the comment section

--- js/bootstrap/bootstrap.bundle.js
+++ js/bootstrap/bootstrap.bundle.js
@@ -11,7 +11,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v5.3.0-alpha1): util/index.js
+   * Bootstrap (v5.3.0-alpha1): util/boostrap.bundle.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
--- js/bootstrap/bootstrap.js
+++ js/bootstrap/bootstrap.js
@@ -30,7 +30,7 @@
 
   /**
    * --------------------------------------------------------------------------
-   * Bootstrap (v5.3.0-alpha1): util/index.js
+   * Bootstrap (v5.3.0-alpha1): util/bootstrap.js
    * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
    * --------------------------------------------------------------------------
    */
