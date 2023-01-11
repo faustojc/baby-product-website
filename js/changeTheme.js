0aa0e209d2fa42a8863ee667b959541fd2673e0c
faustojc <faustojcboko@gmail.com>
2023-01-11 22:44:43 +0800

    Added the change theme code

--- js/changeTheme.js
+++ js/changeTheme.js
@@ -1,4 +1,10 @@
 let buttonChangeTheme = document.querySelector("#changeTheme");
+let body = document.querySelector("body");
+let currTheme = body.getAttribute("data-bs-theme");
+let themeIcon = document.querySelector("#themeIcon");
+
+// Set the text info in changeTheme id to the current theme
+document.querySelector("#changeTheme").textContent = currTheme.toUpperCase();
 
 // Change the theme when clicked
 buttonChangeTheme.addEventListener('click', function () {
@@ -6,6 +12,17 @@ buttonChangeTheme.addEventListener('click', function () {
     body.style.transition = "all 0.3s";
 
     // Change the data-bs-theme attribute
-    changeTheme();
+    if (currTheme === "light") {
+        // Change to dark theme
+        body.setAttribute("data-bs-theme", "dark");
+        themeIcon.classList.replace("bi-sun-fill", "bi-moon-fill");
+    }
+    else {
+        // Change to light theme
+        body.setAttribute("data-bs-theme", "light");
+        themeIcon.classList.replace("bi-moon-fill", "bi-sun-fill");
+    }
+
+    currTheme = body.getAttribute("data-bs-theme");
     buttonChangeTheme.textContent = (currTheme === "light") ? "LIGHT" : "DARK";
 });
