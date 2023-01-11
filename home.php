f22eadfd96594e6a536661cab7d1b430d0cc1c85
faustojc <faustojcboko@gmail.com>
2023-01-12 03:17:45 +0800

    Added random product display

--- home.php
+++ home.php
@@ -49,7 +49,6 @@ if (isset($_POST['logout'])) {
 
     <!-- Custom CSS-->
     <link rel="stylesheet" href="css/style.css" />
-    <link rel="stylesheet" href="css/home.css"/>
  
 </head>
 
@@ -138,64 +137,8 @@ if (isset($_POST['logout'])) {
             </div>
 
 
-            <div class="d-flex justify-content-between align-items-center flex-column flex-lg-row my-5" id="new">
-                <div class="card m-2" id="prodimg" >
-                    <a href="./product.html">
-                    <img src="images/p7.png" class="card-img-top" height="300" alt="Product"  />
-                    </a>
-                    <div class="card-body" >
-                        <p class="card-text fw-bold text-black" >
-                             Bear Onesie
-                        </p>
-                        <small class="text-secondary">
-                            ₱900.00
-                        </small>
-                    </div>
-                </div>
+            <div class="d-flex justify-content-between align-items-center flex-column flex-lg-row my-5" id="mostPopular">
 
-                
-
-                <div class="card m-2" id="prodimg">
-                    <a href="product.html">
-                    <img src="images/p4.png" class="card-img-top" height="300" alt="Product"/>
-                    </a>
-                    <div class="card-body">
-                        <p class="card-text fw-bold text-black">
-                            Wooden High Chair
-                        </p>
-                        <small class="text-secondary">
-                            ₱1,700.00
-                        </small>
-                    </div>
-                </div>
-
-                <div class="card m-2" id="prodimg">
-                    <a href="product.html">
-                    <img src="images/p9.png" class="card-img-top" height="300" alt="Product"/>
-                    </a>
-                    <div class="card-body">
-                        <p class="card-text fw-bold text-black">
-                            Pink Overalls
-                        </p>
-                        <small class="text-secondary">
-                            ₱950.00
-                        </small>
-                    </div>
-                </div>
-
-                <div class="card m-2" id="prodimg">
-                    <a href="product.html">
-                    <img src="images/p1.png" class="card-img-top" height="300" alt="Product"/>
-                    </a>
-                    <div class="card-body">
-                        <p class="card-text fw-bold text-black">
-                            Crib Mobil
-                        </p>
-                        <small class="text-secondary">
-                            ₱1,500.00
-                        </small>
-                    </div>
-                </div>
             </div>
 
             <a href="#" class="btn btn-outline-dark my-5"> View All Products</a>
@@ -206,32 +149,13 @@ if (isset($_POST['logout'])) {
                 </h2>
             </div>
 
-            <div class="d-flex justify-content-between align-items-center flex-column flex-lg-row my-5">
-                <div class="position-relative m-2">
-                    <img src="images/p8.png" height="300" alt="Product" id="rec"/>
-                    <a href="#" class="btn btn-light position-absolute start-1 bottom-0 ms-2 mb-2 d-block" id="view">View</a>
-                </div>
-                <div class="position-relative m-2">
-                    <img src="images/p2.png" height="300" alt="Product" id="rec"/>
-                    <a href="#" class="btn btn-light position-absolute start-1 bottom-0 ms-2 mb-2 d-block" id="view">View</a>
-                </div>
-                <div class="position-relative m-2">
-                    <img src="images/p12.png" height="300" alt="Product" id="rec"/>
-                    <a href="#" class="btn btn-light position-absolute start-1 bottom-0 ms-2 mb-2 d-block" id="view">View</a>
-                </div>
-                 <div class="position-relative m-2">
-                    <img src="images/p15.png" height="300" alt="Product" id="rec"/>
-                    <a href="#" class="btn btn-light position-absolute start-1 bottom-0 ms-2 mb-2 d-block" id="view">View</a>
-                </div>
-                 <div class="position-relative m-2">
-                    <img src="images/p6.png" height="300" alt="Product" id="rec"/>
-                    <a href="#" class="btn btn-light position-absolute start-1 bottom-0 ms-2 mb-2 d-block" id="view">View</a>
-                </div>
+            <div class="d-flex justify-content-between align-items-center flex-column flex-lg-row my-5" id="recentProducts">
+
             </div>
 
             <div class="row text-start align-items-center gy-5 my-5">
                 <div class="col-12 col-md-6">
-                    <img src="images/product3.png" class="w-100 h-100" id="sale">
+                    <img src="images/product3.png" class="w-100 h-100" id="sale" alt="product">
                 </div>
                 <div class="col-12 col-md-6">
                     <div id="Promo">
@@ -245,12 +169,16 @@ if (isset($_POST['logout'])) {
         </div>
 
         <!-- Footer -->
-        <footer class="bg-body-tertiary py-4 mt-5 w-100">
+        <footer class="bg-body py-4 mt-5 w-100 border-top border-secondary" style="border-width: 20px!important;">
             <div class="container">
                 <div class="row justify-content-between align-items-center">
                     <div class="col-md-3">
                         <h3>Angelcare</h3>
-                        <p>Providing your baby needs.</p>
+                        <p class="mb-2">Providing your baby needs.</p>
+                        <p class="mb-2">
+                            Change theme:
+                            <span id="changeTheme" class="badge text-wrap text-bg-primary w-auto" role="button"></span>
+                        </p>
                     </div>
                     <div class="col-md-3">
                         <h3>Links</h3>
@@ -289,8 +217,10 @@ if (isset($_POST['logout'])) {
         </footer>
     </div>
 
-    <script src="js/main.js"></script>
-    <script src="js/changeTheme.js"></script>
+    <script defer type="application/json" src="json/products.json"></script>
+    <script defer src="js/main.js"></script>
+    <script defer src="js/products.js"></script>
+    <script defer src="js/changeTheme.js"></script>
 </body>
 
 </html>
